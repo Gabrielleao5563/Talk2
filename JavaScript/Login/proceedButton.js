4fc3a4eb863903538235c3055bfb043bd212e082
Gabrielleao5563 <92538581+Gabrielleao5563@users.noreply.github.com>
2025-05-28 13:41:16 -0300

    Update
    Now its possible to sign up

--- JavaScript/Login/proceedButton.js
+++ JavaScript/Login/proceedButton.js
@@ -1,7 +1,7 @@
 //Function that runs when the continue button is pressed
-function proceedProcess(){
+function proceedProcess(condition){
 
-    switch(mode){
+    switch(condition){
 
         //Login
         case 1:
@@ -9,12 +9,145 @@ function proceedProcess(){
 
         //Signup
         case 2:
+
+            //Vars about the email
+            var inputEmail = document.getElementById("signupEmailInput");
+
+            //about email security
+            var emailHint = document.getElementById("signupHint1");
+
+            function changeEmailHintConfiguration(action, value){
+
+                switch(action){
+
+                    case "text":
+
+                        emailHint.innerHTML=value;
+                        break;
+
+                    case "color":
+
+                        emailHint.style.backgroundColor=value;
+                        break;
+
+                    case "reset":
+                        break;
+
+                }
+
+                if(action == "reset"){
+
+                    emailHint.style.display="none";
+
+                }else{
+
+                    emailHint.style.display="block";
+
+                }
+
+            }
+
+            //Vars about the password
+            var inputPassword1 = document.getElementById("signupPass1");
+            var inputPasswrod2 = document.getElementById("signupPass2");
+
+            //about password security
+            var passwordHint1 = document.getElementById("signupHint2");
+            var passwordHint2 = document.getElementById("signupHint3");
+            var passwordHint3 = document.getElementById("signupHint4");
+            var passwordHint4 = document.getElementById("signupHint5");
+            var passwordHint5 = document.getElementById("signupHint6");
+            var passwordHint6 = document.getElementById("signupHint7");
+
+            //EMAIL CHECKING ---------------
+
+            //If the email field is empty
+            if(inputEmail.value == ""){
+
+                changeEmailHintConfiguration("text", "Este campo não pode estar vazio!");
+                changeEmailHintConfiguration("color", "var(--warningColor)");
+                return;
+
+            }
+
+            changeEmailHintConfiguration("reset");
+
+            //Check if there is some " " on the email
+            for(i = inputEmail.value.length -1; i >= 0; i--){
+
+                var character = inputEmail.value.charAt(i);
+
+                if(character == " "){
+
+                    changeEmailHintConfiguration("text", "Espaços não são aceitos");
+                    changeEmailHintConfiguration("color", "var(--warningColor)");
+                    return;
+
+                }else{
+                    
+                    changeEmailHintConfiguration("reset");
+
+                }
+
+            }
+
+            //PASWORD CHECKING ---------------
+            for(i = 7; i > 1; i--){
+
+                var hintOnCheck = document.getElementById(String("signupHint" + i));
+
+                if(hintOnCheck.style.backgroundColor == "var(--sucessColor)" | hintOnCheck.style.backgroundColor == "var(--attentionColor)"){
+
+                    //Do nothing
+
+                }else{
+
+                    return;
+
+                }
+
+            }
+
+            //SUPABASE CHECKINGS ---------------
+
+            var email = inputEmail.value;
+            var password = inputPassword1.value;
+
+            async function registerUser(){
+
+                //Send the data to server
+                const { data, error } = await supabaseclient.auth.signUp({
+
+                    email,
+                    password
+
+                });
+
+                //Check if there is a error
+                if(error){
+
+                    console.error("Ocorreu um erro durante o cadastro: " + error.message);
+                    changeEmailHintConfiguration("text", error.message);
+                    changeEmailHintConfiguration("color", "var(--warningColor)");
+
+                }else{
+
+                    console.log("O usuário foi cadastrado: " + data);
+                    changeEmailHintConfiguration("text", "Sucesso! Verifique seu e-mail");
+                    changeEmailHintConfiguration("color", "var(--sucessColor)");
+
+                }
+
+            }
+
+            registerUser();
+
             break;
 
         //Error
         default:
 
-            console.error("The page is actually on a invalid position to continue");
+            console.error("The page is actually on a invalid position to continue! Mode: " + mode + "!");
             break;
 
     }
